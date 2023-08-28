deda2701fe1ecfd0eccff5a01aea0629e3559133
joaoBrRj91 <joao1991algoritmo@gmail.com>
2023-08-28 19:57:33 -0300

    EmailTagHelper

---
+++ DevIO.UI.Site/Extensions/CustomTagHelpers/EmailTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Net.Mime;
+
+namespace DevIO.UI.Site.Extensions.CustomTagHelpers
+{
+
+    public class EmailTagHelper : TagHelper
+    {
+        public string EmailDomain { get; set; } = "meudominio.com";
+
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = $"{content.GetContent()}@{EmailDomain}";
+            output.Attributes.SetAttribute("href", $"mailto:{target}");
+            output.Content.SetContent(target);
+        }
+    }
+}
